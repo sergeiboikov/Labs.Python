ecd762301e1b82762303b06de341355811ddc50c
Sergei Boikov <Sergei_Boikov@epam.com>
2022-06-01 10:38:59 +0400

    Modified RegEx case. Added validating links from Markdown files

--- RegEx/test.py
+++ RegEx/test.py
@@ -1,8 +1,40 @@
-import sys
+from pathlib import Path
 import re
+import os
+import urllib.request as u
 
-regexp = '.*?(?=.*\\bID=\"(\\d+)\")(?=.*\\bUserID=\"(\\d+)\").*'
+"""
+Validates links from Markdown file
+:return: If file exists or link is available returns True, else - False
+:rtype: boolean
+"""
+    
+INLINE_LINK_RE = re.compile(r'\[([^\]]+)\]\(([^)]+)\)')
+root_path = r"D:\Courses\BILab_2"
 
-for line in sys.stdin:
-   if not re.match(regexp, line.strip()):
-       print line.strip()
+def find_md_links(md: str):
+    """ Return dict of links in markdown """
+    links = dict(INLINE_LINK_RE.findall(md))
+    return links
+
+def internet_on(url: str):
+    """ Check that internet resource is available"""
+    try:
+        u.urlopen(url, timeout=1)
+        return True
+    except u.URLError as err: 
+        return False
+
+with open(r"D:\Courses\BILab_2\README.md", "r", encoding="UTF-8") as f:
+    lines = "".join(f.readlines())
+    md_links_dict = find_md_links(lines)
+    for link in md_links_dict.values():
+        # Validate that local file exists
+        if not str(link).startswith("http"):
+            link = str.replace(link, "%20", " ")
+            link_path = Path(os.path.join(root_path, link))
+            if not link_path.is_file():
+                print(f"IS NOT FOUND: {link_path}")
+        else:
+            if not internet_on(link):
+                print(f"IS NOT FOUND: {link}")
--- TransformCsv/TransformCsv.py
+++ TransformCsv/TransformCsv.py
@@ -1,31 +1,35 @@
 import itertools as it
 import sys
 import os
+
 status = None
 date = None
+source_file = r'D:\OneDrive\OneDrive - EPAM\BI Exchange\2021\Databricks\Source files\CoordStatusMA_Short.csv'
+column_names = ['Status', 'Date', 'Latitude', 'Longitude', 'Count']
 
-if os.path.exists(sys.argv[1]):
-	new_file = open(os.path.splitext(sys.argv[1])[0] + '_prepared.csv', 'w')
-
-	with open(sys.argv[1], mode='r', encoding='cp1252') as f:
-		for line in f.readlines():
-			if len(line.strip()) > 0:
-				if line.startswith('Status='):
-					status_line = line.split(',')[0]
-					status = status_line.split('=')[1]
-				else:
-					#Get date
-					date = line.split(',')[0]
-					#Get list with Latitudes
-					_la = line.split(',')[1::3]
-					#Get list with Longitudes
-					_lo = line.split(',')[2::3]
-					#Get list with counts
-					_c = line.split(',')[3::3]
-					if len(_la) == len(_lo) == len(_c):
-						#Aggregate elements from lists
-						for (la, lo, c) in zip(_la, _lo, _c):
-							#while lists are not empty
-							if len(la) > 0 and len(lo) > 0 and len(c) > 0:
-								new_file.write(status.strip() + "," + date.strip() + "," + la.strip() + "," + lo.strip() + "," + c.strip() +"\n")
-	new_file.close()
+if os.path.exists(source_file):
+    new_file = open(os.path.splitext(source_file)[0] + '_prepared.csv', 'w')
+    new_file.write(f'{column_names[0]}, {column_names[1]}, {column_names[2]}, {column_names[3]}, {column_names[4]}\n')
+    with open(source_file, mode='r', encoding='cp1252') as f:
+        for line in f.readlines():
+            if len(line.strip()) > 0:
+                if line.startswith('Status='):
+                    status_line = line.split(',')[0]
+                    status = status_line.split('=')[1]
+                else:
+                    # Get date
+                    date = line.split(',')[0]
+                    # Get list with Latitudes
+                    _la = line.split(',')[1::3]
+                    # Get list with Longitudes
+                    _lo = line.split(',')[2::3]
+                    # Get list with counts
+                    _c = line.split(',')[3::3]
+                    if len(_la) == len(_lo) == len(_c):
+                        # Aggregate elements from lists
+                        for (la, lo, c) in zip(_la, _lo, _c):
+                            # while lists are not empty
+                            if len(la) > 0 and len(lo) > 0 and len(c) > 0:
+                                new_file.write(
+                                    f'{status.strip()}, {date.strip()}, {la.strip()}, {lo.strip()}, {c.strip()}\n')
+    new_file.close()
